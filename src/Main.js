58a5f2b8e59b72947f3882429a1ec2567d957d42
danWagar <dandwagar@gmail.com>
2019-10-15 12:06:57 -0700

    cleaner solution

--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,12 @@ import Note from './Note';
 
 export default class Main extends Component {
   render() {
-    console.log(this.props.id);
+    console.log(this.props);
     return (
       <ul className="noteContainer">
-        {this.props.notes.map(folder => {
-          return folder.map(note => (
-            <Note key={note.id} name={note.name} date={note.modified} content={note.content} />
-          ));
-        })}
+        {this.props.notes.map(note => (
+          <Note key={note.id} name={note.name} date={note.modified} content={note.content} />
+        ))}
       </ul>
     );
   }
